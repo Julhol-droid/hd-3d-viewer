4af81e3604dbbfcdd0ae28ccd25eba3700bd9417
julhol-droid <julian.holzmayer@gmail.com>
2023-08-14 17:15:18 +0200

    Initial Commit: Set up Plugin Headers

---
+++ hd-3d-viewer.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * Plugin Name: 3D Viewer
+ * Description: This is a plugin which allows you to display 3D Objects at your Wordpress Site
+ * Author:      Julian Holzmayer
+ * Author URI:  https://www.holzmayer.dev
+ * License:     TODO
+ * License URI: TODO
+ * Text Domain: hd-3d-viewer
+ * Domain Path: TODO
+ * Version:     0.0.1
+ */
+
